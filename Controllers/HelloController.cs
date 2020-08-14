07f612e11dd4331ff4f26748f4ecc86bd2e8bbd7
niteshrestha <4751915+niteshrestha@users.noreply.github.com>
2020-08-14 15:39:42 +0545

    added HelloController

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_core_heroku.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string name)
+        {
+            return Ok(new { message = $"Hello, {name}" });
+        }
+    }
+}
